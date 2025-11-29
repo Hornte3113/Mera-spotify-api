77a96505d5df216e5a0d3186193fa5896beb5534
Hornte3113 <alix.montesinos@gmail.com>
2025-11-28 22:42:07 -0600

    PONIENDO LAS CREDENCIALES DE AWS

--- src/main/kotlin/com/example/services/S3Service.kt
+++ src/main/kotlin/com/example/services/S3Service.kt
@@ -1,6 +1,7 @@
 package com.example.com.example.services
 
 
+
 import aws.sdk.kotlin.services.s3.S3Client
 import aws.sdk.kotlin.services.s3.model.GetObjectRequest
 import aws.sdk.kotlin.services.s3.model.PutObjectRequest
@@ -14,24 +15,25 @@ import java.util.UUID
 import kotlin.time.Duration.Companion.hours
 
 class S3Service(config: ApplicationConfig) {
-    // Leemos las credenciales del archivo application.yaml
     private val bucketName = config.property("aws.bucketName").getString()
     private val region = config.property("aws.region").getString()
     private val accessKey = config.property("aws.accessKey").getString()
     private val secretKey = config.property("aws.secretKey").getString()
+    // Leemos el token de sesión (puede ser nulo si fuera una cuenta real, pero aquí es obligatorio)
+    private val sessionToken = config.propertyOrNull("aws.sessionToken")?.getString()
 
     // Configuración de autenticación para AWS
     private fun getCredentialsProvider(): CredentialsProvider {
         return object : CredentialsProvider {
             override suspend fun resolve(attributes: Attributes): Credentials {
-                return Credentials(accessKey, secretKey)
+                // AQUÍ ESTÁ EL CAMBIO: Pasamos el sessionToken
+                return Credentials(accessKey, secretKey, sessionToken)
             }
         }
     }
 
-    // 1. SUBIR ARCHIVO (Imagen o Audio)
+    // 1. SUBIR ARCHIVO
     suspend fun uploadFile(fileName: String, fileBytes: ByteArray, contentType: String): String {
-        // Generamos un nombre único para no sobrescribir archivos (ej: "uuid-foto.jpg")
         val uniqueName = "${UUID.randomUUID()}-$fileName"
 
         S3Client.fromEnvironment {
@@ -47,11 +49,10 @@ class S3Service(config: ApplicationConfig) {
             s3.putObject(request)
         }
 
-        // Devolvemos el nombre único (Key) para guardarlo en la base de datos
         return uniqueName
     }
 
-    // 2. GENERAR URL TEMPORAL (Para ver imágenes privadas)
+    // 2. GENERAR URL TEMPORAL
     suspend fun getPresignedUrl(objectKey: String): String {
         S3Client.fromEnvironment {
             this.region = this@S3Service.region
@@ -61,8 +62,6 @@ class S3Service(config: ApplicationConfig) {
                 bucket = bucketName
                 key = objectKey
             }
-
-            // La URL será válida por 12 horas
             val presignedRequest = s3.presignGetObject(request, duration = 12.hours)
             return presignedRequest.url.toString()
         }
